b64d24b7ba03ea3dbd37072e28541af013384030
Sebastiao siqueira <sebastiao.valfrides@gmail.com>
2026-03-20 15:41:35 -0400

    eventos

---
+++ Tarefas/Tarefas/Entities/Tarefa.cs
@@ -0,0 +1,10 @@
+﻿namespace Tarefas.Entities
+{
+    public class Tarefa
+    {
+        public Guid ID { get; set; }
+        public string  Descricao { get; set; }
+        public bool Concluida { get; set; }
+        public DateTime DataCriacao{ get; set; }
+    }
+}
