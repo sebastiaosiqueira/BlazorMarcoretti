eb583756073a7977c81da032f815e87b6b975275
Sebastiao siqueira <sebastiao.valfrides@gmail.com>
2026-04-22 14:04:34 -0400

    lanchesweb

---
+++ LanchesMauiBlazor/LanchesMauiBlazor/Data/LancheService.cs
@@ -0,0 +1,26 @@
+﻿using LanchesLibrary;
+using LanchesLibrary.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LanchesMauiBlazor.Data
+{
+    public class LancheService :ILancheService
+    {
+
+        public async Task<IEnumerable<Lanche>> LoadLanchesAsync()
+        {
+            await Task.Delay(1000);
+            var lanches = new[]
+            {
+        new Lanche("Cheese Burger", "Pão e Hamburger tradicional com queijo"),
+        new Lanche("Cheese Salada", "Pão e Hamburger tradicional com queijo e alface"),
+        new Lanche("Cheese Salada Egg", "Pão e Hamburger tradicional com queijo e alface e ovo"),
+        new Lanche("Misto Quente", "Pão de forma, presunto e queijo"),
+        new Lanche("Bauru", "Pão de forma, presunto, queijo, tomate e queijo"),
+        };
+            return lanches;
+        }
+    }
+}
---
+++ LanchesMauiBlazor/LanchesWeb/Data/LancheService.cs
@@ -0,0 +1,17 @@
+﻿using LanchesLibrary;
+using LanchesLibrary.Data;
+
+namespace LanchesWeb.Data
+{
+    public class LancheService : ILancheService
+    {
+        private readonly HttpClient _httpClient = new();
+
+        public async Task<IEnumerable<Lanche>>? LoadLanchesAsync()
+        {
+            var lanches = await _httpClient.GetFromJsonAsync<List<Lanche>>(
+                "https://www.macoratti.net/Sistemas/lanches.json");
+            return lanches!.ToList();
+        }
+    }
+}
